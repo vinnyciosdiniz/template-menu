e1ee3c7221f7dffa6af2b5c10027473aeb96efdb
Vinnycios de Jesus Gusmao Diniz <vinnycios.diniz@ebserh.gov.br>
2022-10-27 12:17:35 -0300

    inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layout')
+@section('menu')
+    {{-- <div class="topo">
+        <div class="menubtn">
+            <a href="#"><i class="fa-solid fa-bars"></i></a>
+        </div>
+    </div> --}}
+
+    <nav class="sidebar">
+        <div class="text">Menu</div>
+        <ul>
+            <li><a href="#">Opc 1</a></li>
+            <li>
+                <a href="#">Opc 2
+                    <span class="fas fa-caret-down"></span>
+                </a>
+                <ul>
+                    <li><a href="#">intro 1</a></li>
+                    <li><a href="#">intro 2</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Opc 3
+                    <span class="fas fa-caret-down"></span>
+                </a>
+                <ul>
+                    <li><a href="#">intro 3</a></li>
+                    <li><a href="#">intro 4</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Opc2</a></li>
+        </ul>
+    </nav>
+
+    <script></script>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+
+
+Route::get('/', [HomeController::class, 'home']);
