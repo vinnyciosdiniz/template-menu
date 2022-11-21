5c505e1338d2a1a7f988efdbf26ad2885dc16c8f
Jucimara Pereira <maricimara98@outlook.com>
2022-11-21 09:41:37 -0300

    feat: add formulário de pesquisa

--- resources/views/layout3.blade.php
+++ resources/views/layout3.blade.php
@@ -1,48 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  
+    <title>Menu</title>
+
     <script src="https://kit.fontawesome.com/f4ecad88e2.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/estilo2.css">
-    <script src="js/script2.js"></script>
-  
-      <link rel="apple-touch-icon" href="https://estacionamento.huufma.br/static/assets/images/apple-touch-icon.png">
-        <link rel="shortcut icon" href="https://estacionamento.huufma.br/static/assets/images/favicon.png">
+    <link rel="stylesheet" href="css/estilo.css">
+    <script src="js/script.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins&family=Rubik:ital@1&family=Ubuntu:wght@700&display=swap"
+        rel="stylesheet">
 
-        <!-- Stylesheets -->
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/css/bootstrap-extend.min.css">
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/css/site.min.css">
-
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/vendor/animsition/animsition.css">
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/vendor/asscrollable/asScrollable.css">
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/vendor/switchery/switchery.css">
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/vendor/intro-js/introjs.css">
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/vendor/slidepanel/slidePanel.css">    
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/vendor/bootstrap-sweetalert/sweet-alert.css">
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/vendor/bootstrap-datetimepicker/bootstrap-datetimepicker.min.css"  type="text/css"/>
-
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/custom/css/custom.css">
-
-        <!-- Plugin -->
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/vendor/formvalidation/formValidation.min.css">
-        <link href="https://estacionamento.huufma.br/static/assets/vendor/bootstrap-table/bootstrap-table.css" rel="stylesheet" type="text/css"/>
-        <link href="https://estacionamento.huufma.br/static/assets/vendor/select2/select2.min.css" rel="stylesheet" type="text/css"/>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/fonts/web-icons/web-icons.min.css">  
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/fonts/brand-icons/brand-icons.min.css">
-        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,300italic'>
-        <link rel="stylesheet" href="https://estacionamento.huufma.br/static/assets/fonts/font-awesome/font-awesome.min.css"  type="text/css"/>
-
-        {{-- TESTE --}}
-
-        <title>Menu</title>
 </head>
+
 <body>
     @yield('menu')
 </body>
+
 </html>
--- resources/views/menu3.blade.php
+++ resources/views/menu3.blade.php
@@ -1,104 +1,118 @@
 @extends('layout3')
 @section('menu')
-    <nav class="site-navbar navbar navbar-default navbar-fixed-top navbar-mega" role="navigation">
-
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle hamburger hamburger-close navbar-toggle-left unfolded"
-                data-toggle="menubar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="hamburger-bar"></span>
-            </button>
-            <button type="button" class="navbar-toggle collapsed" data-target="#site-navbar-collapse" data-toggle="collapse">
-                <i class="icon wb-more-horizontal" aria-hidden="true"></i>
-            </button>
-            <button type="button" class="navbar-toggle collapsed" data-target="#site-navbar-search" data-toggle="collapse">
-                <span class="sr-only">Toggle Search</span>
-                <i class="icon wb-search" aria-hidden="true"></i>
-            </button>
-            <div class="navbar-brand navbar-brand-center">
-                <!--<img class="navbar-brand-logo" src="https://estacionamento.huufma.br/static/assets/images/logo_black.png" title="Serviço Residencial">-->
-                <span class="icon fa-car fa-3x"></span>
-                <span class="navbar-brand-text">SIS Estacionamento</span>
-            </div>
+    {{-- <div class="topo">
+        <div class="menubtn">
+            <a href="#"><i class="fa-solid fa-bars"></i></a>
         </div>
+    </div> --}}
 
-        <div class="navbar-container container-fluid">
-            <!-- Navbar Collapse -->
-            <div class="collapse navbar-collapse navbar-collapse-toolbar" id="site-navbar-collapse">
-                <!-- Navbar Toolbar -->
-                <ul class="nav navbar-toolbar">
-                    <li class="hidden-float" id="toggleMenubar">
-                        <a data-toggle="menubar" href="#" role="button">
-                            <i class="icon hamburger hamburger-arrow-left unfolded">
-                                <span class="sr-only">Toggle menubar</span>
-                                <span class="hamburger-bar"></span>
-                            </i>
-                        </a>
-                    </li>
-                    <li class="hidden-xs" id="toggleFullscreen">
-                        <a class="icon icon-fullscreen" data-toggle="fullscreen" href="#" role="button">
-                            <span class="sr-only">Toggle fullscreen</span>
-                        </a>
-                    </li>
-
-                </ul>
-                <!-- End Navbar Toolbar -->
-
-                <!-- Navbar Toolbar Right -->
-                <ul id="nav-toolbar-user" class="nav navbar-toolbar navbar-right navbar-toolbar-right hide">
-
-                    <li class="dropdown">
-                        <a class="navbar-avatar dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false"
-                            data-animation="slide-bottom" role="button">
-                            <span class="avatar avatar-online">
-                                <img src="https://estacionamento.huufma.br/static/assets/images/user.png" alt="...">
-                                <i></i>
-                            </span>
-                            <label></label>
-                        </a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li role="presentation">
-                                <a href="https://estacionamento.huufma.br/login/sair" role="menuitem"><i
-                                        class="icon wb-power" aria-hidden="true"></i> Sair</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-                <!-- End Navbar Toolbar Right -->
-            </div>
-            <!-- End Navbar Collapse -->
-
-            <!-- Site Navbar Seach -->
-            <div class="collapse navbar-search-overlap" id="site-navbar-search">
-                <form role="search">
-                    <div class="form-group">
-                        <div class="input-search">
-                            <i class="input-search-icon wb-search" aria-hidden="true"></i>
-                            <input type="text" class="form-control" name="site-search" placeholder="Search...">
-                            <button type="button" class="input-search-close icon wb-close"
-                                data-target="#site-navbar-search" data-toggle="collapse" aria-label="Close"></button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <!-- End Site Navbar Seach -->
-        </div>
-    </nav>
-    <!-- menu lateral -->
-    <div class="site-menubar hide">
-        <div class="site-menubar-body">
+    {{-- Menu --}}
+    <nav class="sidebar">
+        <div class="text">Me</div>
+        <ul id="ul-principal">
             <div>
-                <div>
-                    <ul class="site-menu">
-                        <li class="site-menu-category">Menu Principal</li>
-                        <li class="site-menu-item">
-                            <a href="https://estacionamento.huufma.br/cadastrar" data-slug="cad">
-                                <i class="site-menu-icon wb-add-file" aria-hidden="true"></i>
-                                <span class="site-menu-title">Cadastro</span>
-                            </a>
-                        </li>
-                </div>
+                <li class="li-p">
+                    <a href="#" id="dropbtn" onclick="opc1()">Opc 1
+                        <span class="fas fa-caret-down"></span>
+                    </a>
+                    <ul id="myDropdown1" class="dropdown-content">
+                        <li><a href="#" id="opc" onclick="introUm()">intro 1</a></li>
+                        <li><a href="#" onclick="introDois()">intro 2</a></li>
+                    </ul>
+                </li>
             </div>
-        </div>
+            <div>
+                <li class="li-p">
+                    <a href="#" class="dropbtn" onclick="opc2()">Opc 2
+                        <span class="fas fa-caret-down"></span>
+                    </a>
+                    <ul id="myDropdown2" class="dropdown-content">
+                        <li><a href="#">intro 1</a></li>
+                        <li><a href="#">intro 2</a></li>
+                    </ul>
+                </li>
+            </div>
+            <div>
+                <li class="li-p">
+                    <a href="#" class="dropbtn" onclick="opc3()">Opc 3
+                        <span class="fas fa-caret-down"></span>
+                    </a>
+                    <ul id="myDropdown3" class="dropdown-content">
+                        <li><a href="#">intro 1</a></li>
+                        <li><a href="#">intro 2</a></li>
+                    </ul>
+                </li>
+            </div>
+            <div>
+                <li class="li-p">
+                    <a href="#" class="dropbtn" onclick="opc4()">Opc 4
+                        <span class="fas fa-caret-down"></span>
+                    </a>
+                    <ul id="myDropdown4" class="dropdown-content">
+                        <li><a href="#">intro 1</a></li>
+                        <li><a href="#">intro 2</a></li>
+                    </ul>
+                </li>
+            </div>
+        </ul>
+    </nav>
+    {{-- tela principal --}}
+    <div class="screen">
+        {{-- Iniciando Form Dropwdown --}}
+        <form action="#" >
+            <fieldset class="fieldset">
+                <legend>
+                    <fieldset id="field-legenda">Formulário de busca</fieldset>
+                    
+                </legend>
+                <div class="opcao">
+                    <label for="especialidade">Especialidade</label>
+                    <select name="especialidade" id="especialidade">
+                    <option value="Todas as especialidades">TODAS AS ESPECIALIDADES</option>
+                    </select>
+                </div>
+                <div class="opcao">
+                    <label for="nome">Nome do médico</label>
+                    <select name="nome" id="nome">
+                    <option value="Todas as especialidades">TODOS OS MÉDICOS</option>
+                    </select>
+                </div>
+                <div class="opcao">
+                    <label for="unidade">Unidade</label>
+                    <select id="unidade">
+                    <option value="Todas as especialidades">TODAS AS UNIDADES</option>
+                    <option value="Todas as especialidades">UNIDADE MATERNO INFANTIL</option>
+                    </select>
+                </div>
+                <div class="opcao">
+                    <label for="andar">Andar</label>
+                    <select name="andar" id="andar">
+                    <option value="Todas as especialidades">TODOS OS ANDARES</option>
+                    <option value="Todas as especialidades">1º ANDAR</option>
+                    <option value="Todas as especialidades">2º ANDAR</option>
+                    </select>
+                </div>
+                <div class="opcao">
+                    <label for="ala">Ala</label>
+                    <select name="ala" id="ala">
+                    <option value="Todas as especialidades">TODAS AS ALAS</option>
+                    <option value="Todas as especialidades">AMARELA</option>
+                    <option value="Todas as especialidades">AZUL</option>
+                    <option value="Todas as especialidades">ROSA</option>
+                    <option value="Todas as especialidades">VERDE</option>
+
+                    </select>
+                </div>
+            </fieldset>
+        </form>
+        <input type="submit" value="Limpar" />
+        <input type="submit" value="Buscar" />
+        {{-- Fim Form Dropwdown --}}
     </div>
+        
+    <script>
+         $(document).ready(function() {
+            $('#estado').select2();
+        });
+    </script>
 @endsection
