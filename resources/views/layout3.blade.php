597b6b3842cb6192dd95096ab330b881049a11a0
Jucimara Pereira <maricimara98@outlook.com>
2022-12-15 10:52:36 -0300

    mudando título

--- resources/views/layout3.blade.php
+++ resources/views/layout3.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Pushy - Off-Canvas Navigation Menu</title>
+    <title>Menu Responsivo</title>
     <meta name="description" content="Pushy is an off-canvas navigation menu for your website.">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
