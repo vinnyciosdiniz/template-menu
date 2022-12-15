40d142712dfd7d54a68e1905cc33900beed3fb07
Jucimara Pereira <maricimara98@outlook.com>
2022-12-15 10:30:17 -0300

    Atualizando menu3

--- resources/views/layout3.blade.php
+++ resources/views/layout3.blade.php
@@ -1,25 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Pushy - Off-Canvas Navigation Menu</title>
+        <meta name="description" content="Pushy is an off-canvas navigation menu for your website.">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Menu</title>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/demo.css">
+        <!-- Pushy CSS -->
+        <link rel="stylesheet" href="css/pushy.css">
 
-    <script src="https://kit.fontawesome.com/f4ecad88e2.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/estilo.css">
-    <script src="js/script.js"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins&family=Rubik:ital@1&family=Ubuntu:wght@700&display=swap"
-        rel="stylesheet">
-
-</head>
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    </head>
 
 <body>
-    @yield('menu')
+    @yield('menu3')
 </body>
 
 </html>
--- resources/views/menu3.blade.php
+++ resources/views/menu3.blade.php
@@ -1,118 +1,59 @@
 @extends('layout3')
-@section('menu')
-    {{-- <div class="topo">
-        <div class="menubtn">
-            <a href="#"><i class="fa-solid fa-bars"></i></a>
+@section('menu3')
+
+    <header class="site-header push">This is a header</header>
+
+    <!-- Pushy Menu -->
+    <nav class="pushy pushy-left" data-focus="#first-link">
+        <div class="pushy-content">
+            <ul>
+                <li class="pushy-link"><a href="#">Início</a></li>
+                <li class="pushy-submenu">
+                    <button>Pesquisar</button>
+                    <ul>
+                        <li class="pushy-link"><a href="#">Admitidos</a></li>
+                        <li class="pushy-link"><a href="#">Altas</a></li>
+                        <li class="pushy-link"><a href="#">Internados</a></li>
+                        <li class="pushy-link"><a href="#">Lista de Espera</a></li>
+                    </ul>
+                </li>
+                <li class="pushy-submenu">
+                    <button>Estatisticas</button>
+                    <ul>
+                        <li class="pushy-link"><a href="#">Item 1</a></li>
+                        <li class="pushy-link"><a href="#">Item 2</a></li>
+                        <li class="pushy-link"><a href="#">Item 3</a></li>
+                    </ul>
+                </li>
+                <li class="pushy-submenu">
+                    <button>Mapa Diário</button>
+                    <ul>
+                        <li class="pushy-link"><a href="#">Item 1</a></li>
+                        <li class="pushy-link"><a href="#">Item 2</a></li>
+                        <li class="pushy-link"><a href="#">Item 3</a></li>
+                    </ul>
+                </li>
+                <li class="pushy-link"><a href="#">Declaração</a></li>
+                <li class="pushy-link"><a href="#">Sair</a></li>
+            </ul>
         </div>
-    </div> --}}
-
-    {{-- Menu --}}
-    <nav class="sidebar">
-        <div class="text">Me</div>
-        <ul id="ul-principal">
-            <div>
-                <li class="li-p">
-                    <a href="#" id="dropbtn" onclick="opc1()">Opc 1
-                        <span class="fas fa-caret-down"></span>
-                    </a>
-                    <ul id="myDropdown1" class="dropdown-content">
-                        <li><a href="#" id="opc" onclick="introUm()">intro 1</a></li>
-                        <li><a href="#" onclick="introDois()">intro 2</a></li>
-                    </ul>
-                </li>
-            </div>
-            <div>
-                <li class="li-p">
-                    <a href="#" class="dropbtn" onclick="opc2()">Opc 2
-                        <span class="fas fa-caret-down"></span>
-                    </a>
-                    <ul id="myDropdown2" class="dropdown-content">
-                        <li><a href="#">intro 1</a></li>
-                        <li><a href="#">intro 2</a></li>
-                    </ul>
-                </li>
-            </div>
-            <div>
-                <li class="li-p">
-                    <a href="#" class="dropbtn" onclick="opc3()">Opc 3
-                        <span class="fas fa-caret-down"></span>
-                    </a>
-                    <ul id="myDropdown3" class="dropdown-content">
-                        <li><a href="#">intro 1</a></li>
-                        <li><a href="#">intro 2</a></li>
-                    </ul>
-                </li>
-            </div>
-            <div>
-                <li class="li-p">
-                    <a href="#" class="dropbtn" onclick="opc4()">Opc 4
-                        <span class="fas fa-caret-down"></span>
-                    </a>
-                    <ul id="myDropdown4" class="dropdown-content">
-                        <li><a href="#">intro 1</a></li>
-                        <li><a href="#">intro 2</a></li>
-                    </ul>
-                </li>
-            </div>
-        </ul>
     </nav>
-    {{-- tela principal --}}
-    <div class="screen">
-        {{-- Iniciando Form Dropwdown --}}
-        <form action="#" >
-            <fieldset class="fieldset">
-                <legend>
-                    <fieldset id="field-legenda">Formulário de busca</fieldset>
-                    
-                </legend>
-                <div class="opcao">
-                    <label for="especialidade">Especialidade</label>
-                    <select name="especialidade" id="especialidade">
-                    <option value="Todas as especialidades">TODAS AS ESPECIALIDADES</option>
-                    </select>
-                </div>
-                <div class="opcao">
-                    <label for="nome">Nome do médico</label>
-                    <select name="nome" id="nome">
-                    <option value="Todas as especialidades">TODOS OS MÉDICOS</option>
-                    </select>
-                </div>
-                <div class="opcao">
-                    <label for="unidade">Unidade</label>
-                    <select id="unidade">
-                    <option value="Todas as especialidades">TODAS AS UNIDADES</option>
-                    <option value="Todas as especialidades">UNIDADE MATERNO INFANTIL</option>
-                    </select>
-                </div>
-                <div class="opcao">
-                    <label for="andar">Andar</label>
-                    <select name="andar" id="andar">
-                    <option value="Todas as especialidades">TODOS OS ANDARES</option>
-                    <option value="Todas as especialidades">1º ANDAR</option>
-                    <option value="Todas as especialidades">2º ANDAR</option>
-                    </select>
-                </div>
-                <div class="opcao">
-                    <label for="ala">Ala</label>
-                    <select name="ala" id="ala">
-                    <option value="Todas as especialidades">TODAS AS ALAS</option>
-                    <option value="Todas as especialidades">AMARELA</option>
-                    <option value="Todas as especialidades">AZUL</option>
-                    <option value="Todas as especialidades">ROSA</option>
-                    <option value="Todas as especialidades">VERDE</option>
 
-                    </select>
-                </div>
-            </fieldset>
-        </form>
-        <input type="submit" value="Limpar" />
-        <input type="submit" value="Buscar" />
-        {{-- Fim Form Dropwdown --}}
+    <!-- Site Overlay -->
+    <div class="site-overlay"></div>
+
+    <!-- Your Content -->
+    <div id="container">
+        <!-- Menu Button -->
+        <button class="menu-btn">&#9776; Menu</button>
+
+        <p> Tabela Aqui</p>
+
     </div>
-        
-    <script>
-         $(document).ready(function() {
-            $('#estado').select2();
-        });
-    </script>
+
+    <footer class="site-footer push">This is a footer</footer>
+
+    <!-- Pushy JS -->
+    <script src="js/pushy.js"></script>
+
 @endsection
